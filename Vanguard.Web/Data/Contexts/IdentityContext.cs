4bc57b62c6131cd4e1c8e27b2598efd06dd6a466
Никита Фомин <defdXs@yandex.ru>
2024-11-03 22:42:18 +0300

    Minor fixes that really fixed db connection

--- Vanguard.Web/Data/Contexts/IdentityContext.cs
+++ Vanguard.Web/Data/Contexts/IdentityContext.cs
@@ -7,11 +7,16 @@ namespace Vanguard.Web.Data.Contexts;
 
 public class IdentityContext : IdentityDbContext<User>
 {
-    private readonly YamlConfigService _yamlConfigService;
+    //private readonly YamlConfigService _yamlConfigService;
 
-    public IdentityContext(YamlConfigService yamlConfigService)
+    //public IdentityContext(YamlConfigService yamlConfigService)
+    //{
+    //    _yamlConfigService = yamlConfigService;
+    //}
+
+    public IdentityContext()
     {
-        _yamlConfigService = yamlConfigService;
+        
     }
 
     public async Task SeedDataAsync(IServiceProvider serviceProvider)
@@ -21,8 +26,9 @@ public class IdentityContext : IdentityDbContext<User>
 
     protected override async void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var conf = await _yamlConfigService.LoadSettingsAsync();
-        optionsBuilder.UseNpgsql($"Host={conf.DatabaseHostIP};Port={conf.DatabaseHostPort};Database={conf.DatabaseName};Username={conf.DatabaseUsername};Password={conf.DatabasePassword};Include Error Detail=True");
-        System.Diagnostics.Debug.WriteLine("IdentityContext configured");
+        //var conf = await _yamlConfigService.LoadSettingsAsync();
+
+        //optionsBuilder.UseNpgsql($"Host={conf.DatabaseHostIP};Port={conf.DatabaseHostPort};Database={conf.DatabaseName};Username={conf.DatabaseUsername};Password={conf.DatabasePassword};Include Error Detail=True");
+        optionsBuilder.UseNpgsql($"Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=1234;Include Error Detail=True");
     }
 }
